f49552e21513a1c3054652507e1899825a2af467
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2022-12-16 15:24:58 -0500

    solve 4 excersise

---
+++ ejercicio4.java
@@ -0,0 +1,95 @@
+/*
+ * // Primer ejercicio
+ * -----------------------------------------------------------------------------
+ * -
+ * public class ejercicio4 {
+ * 
+ * public static void main(String[] args) {
+ * double numeroif = -0.9;
+ * 
+ * if (numeroif == 0) {
+ * System.out.println("Su número es cero");
+ * } else if (numeroif > 0) {
+ * System.out.println("Su número es positivo");
+ * } else {
+ * System.out.println("Su número es negativo");
+ * }
+ * 
+ * }
+ * 
+ * }
+ */
+
+// Segundo ejericio
+// ---------------------------------------------------------------------------------
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        }
+
+    }
+}
+
+// Tercer ejercicio
+// ---------------------------------------------------------------------------------
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        int numeroWhile = 0;
+        do {
+            numeroWhile = numeroWhile + 1;
+        } while (numeroWhile < 3);
+        {
+            System.out.println(numeroWhile);
+        }
+    }
+}
+
+// Cuarto ejercicio
+// ---------------------------------------------------------------------------------
+
+/*
+ * public class ejercicio4 {
+ * public static void main(String[] args) {
+ * int numeroFor;
+ * for (numeroFor = 0; numeroFor <= 3; numeroFor++) {
+ * System.out.println(numeroFor);
+ * }
+ * }
+ * }
+ */
+
+// Quinto ejerciocio
+// ------------------------------------------------------------------------------------
+
+/*
+ * public class ejercicio4 {
+ * public static void main(String[] args) {
+ * String estacion = "invierno";
+ * 
+ * switch (estacion) {
+ * case "invierno":
+ * System.out.println("Es invierno, abrigate");
+ * break;
+ * case "verano":
+ * System.out.println("Es verano, podrias comer helado");
+ * break;
+ * case "otoño":
+ * System.out.println("Es otoño, puedes dar un paseo");
+ * break;
+ * case "primavera":
+ * System.out.println("Es primavera, mira ese bello paisaje");
+ * break;
+ * default:
+ * System.out.
+ * println("Por favor, ingresa una estación válida en minuscula y sin tilde");
+ * }
+ * 
+ * }
+ * 
+ * }
+ */
