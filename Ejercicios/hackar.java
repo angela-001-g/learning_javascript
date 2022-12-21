1a409bcf5bad1c6de3ce160b0725fce2ad363f6d
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2022-12-20 20:23:09 -0500

    new exercices

---
+++ Ejercicios/hackar.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hackar {
+    Scanner scanner = new Scanner(System.in);
+    int userInput;{
+        userInput = scanner.nextInt();
+        System.out.println(userInput);
+    }
+
+}
