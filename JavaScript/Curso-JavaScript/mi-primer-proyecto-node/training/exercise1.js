b67f50e56f35236d18908ed3680de68cc6f7c095
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2022-12-29 21:52:56 -0500

    exercise1.1

--- JavaScript/Curso-JavaScript/mi-primer-proyecto-node/training/exercise1.js
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/training/exercise1.js
@@ -4,7 +4,8 @@ const libro = {
     autor: "Gabriel Garcia Marquez", 
     fecha: 1975, 
     url: "https://www.buscalibre.com.co/libro-el-otono-del-patriarca/9788439729839/p/53241527?gclid=CjwKCAiAkrWdBhBkEiwAZ9cdcETihdlhRD6X7CoJS1kypIJrUbQeBzGQF_8QsWCY_AC6aoyiD4VRFBoCTQIQAvD_BwE",
-}
+};
+
 const lista = [ "Angela", 21, false, fecha_valor, libro];
 
 console.log(lista);
