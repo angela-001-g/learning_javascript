d78e4d66215374096616d4cdf227f541e60240b5
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-01-04 02:20:36 -0500

    nuevos cambios

---
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/learning/05-09_JavaScript-OB/numbers.js
@@ -0,0 +1,19 @@
+//Declaracion de variables int y floats 
+let a = 5; 
+console.log(a);
+let b = 0.1; 
+console.log(b);
+
+// Precision 
+
+let c = 0.2; 
+console.log(b + c); // 0.30000000000000004 <-- Imprime eso. 
+//Lo anterior pasa por cómo está almacenado el 0.3000... con el numero de bytes que tiene el lenguaje para esa variable no se puede llegar a la cantidad exacta de 0.3 
+
+
+
+
+
+
+
+
