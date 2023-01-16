743c727ba273d53e32f2caa3dcadaac37b502938
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-01-16 17:37:53 -0500

    09

---
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/learning/09/index.js
@@ -0,0 +1,10 @@
+const logger = require('./logger'); 
+
+
+
+
+ logger.log("Hola, estoy saliendo por pantalla.");
+// logger.info("Hola, esto es un mensaje informativo.");
+ logger.warn("Este es un mensaje de advertemcia.");
+ logger.error("Esto es un error.");
+ logger.debug("Esto es un mensaje de debug.");
