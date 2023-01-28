c89424d81fb9403723fd6bfc9e4199628cf0755f
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-01-27 19:02:48 -0500

    birdWatcher

--- JavaScript/Curso-JavaScript/mi-primer-proyecto-node/ejercicios/algoritmos/08-birdwatcher.js
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/ejercicios/algoritmos/08-birdwatcher.js
@@ -1,9 +1,11 @@
 // 1. Determine the total number of birds that you counted so far 
-function totalBirdCount(birdsPerDay){
-    for(let i=0; i <= birdsPerDay.length; i++){
-        const suma = birdsPerDay.reduce((a,b) => a + b, 0)
-        return suma;
+
+function totalBirdCount(birdsPerDay) {    
+    let suma = 0; 
+    for(let i = 0; i < birdsPerDay.length; i++){
+        suma = birdsPerDay[i] + suma
     }
+    return suma
 }
 // console.log(totalBirdCount([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1]))
 
@@ -19,9 +21,26 @@ function birdsInWeek(birdsPerDay, week){
 
 }
 
-console.log(birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2))
+// console.log(birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2))
 
 // 3. Fix a counting mistake
-function fixBirdCountLog(birdsPerDay){
-    
+// let birdsPerDay = [2, 5, 0, 7, 4, 1]; 
+// // Para recorrer cada elemento de la lista 
+//  for (let i = 0; i < birdsPerDay.length; i++){
+//      console.log(birdsPerDay[i] + 1);
+// }
+let birds = [3, 0, 5, 1, 0, 4, 1, 0, 3, 4, 3, 0]
+
+function fixBirdCountLog(birdsPerDay){ 
+    let newBirds = birdsPerDay
+    for (let i = 0; i < newBirds.length; i++){
+        if(i % 2 === 0){ 
+            newBirds[i] = newBirds[i] + 1
+        }  
+    }
+    return newBirds 
 }
+const a = fixBirdCountLog(birds)
+ //[  4, 0, 6, 1, 1, 4, 2, 0, 4, 4, 4, 0];
+console.log(a)
+console.log(birds)
