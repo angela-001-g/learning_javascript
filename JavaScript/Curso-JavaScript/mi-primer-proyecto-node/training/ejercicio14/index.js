dab8d495cac0a4a7c4044139e16ee99504048142
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-06-28 14:01:54 -0500

    ejercicio 14

---
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/training/ejercicio14/index.js
@@ -0,0 +1,9 @@
+const boton = document.querySelector("#btn")
+const boton2 = document.querySelector("#btn-2")
+
+boton.addEventListener("click", ()=>{
+    alert("click en el botón")
+})
+boton2.addEventListener("click", ()=>{
+    console.log("Hola, estsoy utilizando jQuery")
+})
