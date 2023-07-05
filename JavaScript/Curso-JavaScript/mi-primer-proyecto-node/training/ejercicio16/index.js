8475f9ec4280f6f2f42b5258806446e0d0431e4e
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-04 23:31:27 -0500

    just to be sure

--- JavaScript/Curso-JavaScript/mi-primer-proyecto-node/training/ejercicio16/index.js
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/training/ejercicio16/index.js
@@ -1,7 +1,7 @@
 const piezas = document.querySelector("#cuadricula-piezas")
 
 piezas.forEach(pieza => {
-    pieza.addEventListener("dragstart", () => {
+    pieza.addEventListener("dragstart", pieza => {
         console.log("esto arrastrando una ficha")
     })
 })
