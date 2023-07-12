098c34074678ac07330618c801776defba88b671
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-12 18:43:39 -0500

    little advance

---
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/ejercicios/hackerrank/35-towerBreakers.js
@@ -0,0 +1,18 @@
+function towerBreakers(n, m) {
+
+let tunrsCounter = 0;
+let torres = []
+
+for(let i = 0; i < n; i++){
+    torres.push(m)
+}
+console.log(torres)
+const torreFinal = torres.map(x => { 
+    
+})
+
+console.log(torreFinal)
+
+
+}
+towerBreakers(2, 6)
