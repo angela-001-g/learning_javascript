09e86e38d7d2b9da812f029b32c01eb1b66a6daa
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-23 20:50:46 -0500

    adelanto

--- JavaScript/Curso-JavaScript/mi-primer-proyecto-node/ejercicios/hackerrank/36-closestNumbers.js
+++ JavaScript/Curso-JavaScript/mi-primer-proyecto-node/ejercicios/hackerrank/36-closestNumbers.js
@@ -17,10 +17,24 @@ arrayPairs.forEach(element => {
         restas.push(resta)
     }
 })
-console.log(arrayPairs)
-console.log(restas)
 
+let newObj ={}
+
+for(let i = 0; i<restas.length; i++){
+!newObj[restas[i]] ? newObj[restas[i]] = arrayPairs[i]:  newObj[restas[i]] += 1
+}
+let final = []
+let claves = Object.keys(newObj)
+
+for(let i = 0; i<claves.length; i++){
+    if(claves[i] === claves[0]){
+        final.push(newObj[claves[i]])
+    }
+}
+let result = final.flat()
+
+return result
 }
 
-// console.log(closestNumbers([5, 4, 3, 2]))
+console.log(closestNumbers([-5, 15, 25, 71, 63]))
 closestNumbers([-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854])
